5e9dbc5374c4aec6f80136724bc1331dcbaa10cc
Mateusz Radko <mr.radzio@gmail.com>
2015-06-02 23:21:17 +0200

    zrefaktoryzowane gui + poprawki stylistyczne w kodzie + rename plikow

---
+++ HerdSimulationGUI.py
@@ -0,0 +1,141 @@
+from Tkinter import *
+import threading
+import SimulationEngine
+
+def start_sim():
+    if not SimulationEngine.IS_RUNNING:
+        SimulationEngine.IS_RUNNING = True
+        t = threading.Thread(target=SimulationEngine.start)
+        t.daemon = True
+        t.start()
+    else:
+        print "Simulation is still running"
+
+
+
+def set_velocity():
+    selection = "MAX_VELOCITY = " + str(velocity_value.get())
+    SimulationEngine.MAX_VELOCITY = velocity_value.get()
+    print selection
+
+
+def set_too_close():
+    selection = "TOO_CLOSE_RANGE = " + str(to_close_val.get())
+    SimulationEngine.TOO_CLOSE_RANGE = to_close_val.get()
+    print selection
+
+
+def set_sight_range():
+    selection = "SIGHT_RANGE = " + str(sight_range_value.get())
+    SimulationEngine.SIGHT_RANGE = sight_range_value.get()
+    print selection
+
+
+def set_rule1():
+    selection = "RULE1_DIVIDER = " + str(rule_1_val.get())
+    SimulationEngine.RULE1_DIVIDER = rule_1_val.get()
+    print selection
+
+
+def set_rule3():
+    selection = "RULE3_DIVIDER = " + str(rule_3_val.get())
+    SimulationEngine.RULE3_DIVIDER = rule_3_val.get()
+    print selection
+
+############################################################
+
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+velocity_value = DoubleVar()
+to_close_val = DoubleVar()
+sight_range_value = DoubleVar()
+rule_1_val = DoubleVar()
+rule_3_val = DoubleVar()
+
+############################################################
+
+vel_frame = Frame(frame)
+vel_frame.pack(side=TOP)
+
+velocity_label = Label(vel_frame, text="Max velocity")
+velocity_label.pack(side=LEFT)
+
+velocity_scale = Scale(vel_frame, from_=1, to=15, orient=HORIZONTAL, length=300, variable=velocity_value)
+velocity_scale.pack(side=LEFT)
+velocity_scale.set(SimulationEngine.MAX_VELOCITY)
+
+velocity_set_bt = Button(vel_frame, text="Set", command=set_velocity)
+velocity_set_bt.pack(side=LEFT)
+
+############################################################
+
+sight_range_frame = Frame(frame)
+sight_range_frame.pack(side=TOP)
+
+sight_range_label = Label(sight_range_frame, text="Sight Range")
+sight_range_label.pack(side=LEFT)
+
+sight_range_scale = Scale(sight_range_frame, from_=1, to=SimulationEngine.MAX_POSITION_X, orient=HORIZONTAL, length=300,
+                          variable=sight_range_value)
+sight_range_scale.pack(side=LEFT)
+sight_range_scale.set(SimulationEngine.SIGHT_RANGE)
+
+sight_range_bt = Button(sight_range_frame, text="Set", command=set_sight_range)
+sight_range_bt.pack(side=LEFT)
+
+############################################################
+
+to_close_frame = Frame(frame)
+to_close_frame.pack(side=TOP)
+
+to_close_label = Label(to_close_frame, text="Too Close Range")
+to_close_label.pack(side=LEFT)
+
+to_close_scale = Scale(to_close_frame, from_=1, to=SimulationEngine.MAX_POSITION_X, orient=HORIZONTAL, length=300,
+                       variable=to_close_val)
+to_close_scale.pack(side=LEFT)
+to_close_scale.set(SimulationEngine.TOO_CLOSE_RANGE)
+
+to_close_bt = Button(to_close_frame, text="Set", command=set_too_close)
+to_close_bt.pack(side=LEFT)
+
+############################################################
+
+
+rule1_frame = Frame(frame)
+rule1_frame.pack(side=TOP)
+
+rule1_label = Label(rule1_frame, text="Rule1 Divider")
+rule1_label.pack(side=LEFT)
+
+rule1_scale = Scale(rule1_frame, from_=1, to=1000, orient=HORIZONTAL, length=300, variable=rule_1_val)
+rule1_scale.pack(side=LEFT)
+rule1_scale.set(SimulationEngine.RULE1_DIVIDER)
+
+rule1_bt = Button(rule1_frame, text="Set", command=set_rule1)
+rule1_bt.pack(side=LEFT)
+
+############################################################
+
+rule3_frame = Frame(frame)
+rule3_frame.pack(side=TOP)
+
+rule3_label = Label(rule3_frame, text="Rule3 Divider")
+rule3_label.pack(side=LEFT)
+
+rule3_scale = Scale(rule3_frame, from_=1, to=1000, orient=HORIZONTAL, length=300, variable=rule_3_val)
+rule3_scale.pack(side=LEFT)
+rule3_scale.set(SimulationEngine.RULE3_DIVIDER)
+
+rule3_bt = Button(rule3_frame, text="Set", command=set_rule3)
+rule3_bt.pack(side=LEFT)
+
+############################################################
+
+start_bt = Button(frame, text="Start", command=start_sim)
+start_bt.pack(side=BOTTOM)
+root.mainloop()
+
+
---
+++ SimulationEngine.py
@@ -0,0 +1,186 @@
+import random
+import pygame
+import math
+import time
+from pygame.locals import *
+
+# CONSTANTS
+
+MAX_POSITION_X = 800
+MIN_POSITION_X = 0
+MAX_POSITION_Y = 600
+MIN_POSITION_Y = 0
+
+white = 255, 255, 255
+
+# PARAMETERS
+
+MAX_VELOCITY = 5
+SIGHT_RANGE = 350
+TOO_CLOSE_RANGE = 20
+
+RULE1_DIVIDER = 100
+RULE3_DIVIDER = 8
+
+WAIT_TIME = 0.1
+
+MIN_VELOCITY = -MAX_VELOCITY
+
+IS_RUNNING = False
+
+class Boid(object):
+    # pozycja i predkosc sa listami dwuelementowymi
+    def __init__(self, position, velocity):
+        self.position = position
+        self.velocity = velocity
+
+    def __str__(self):
+        return str(self.position) + " " + str(self.velocity)
+
+    def __repr__(self):
+        return str(self.position) + " " + str(self.velocity)
+
+    # sprawdzanie, czy dwa boidy leza w swoim zasiegu
+    def in_range(self, another_boid):
+        if math.sqrt(math.pow(self.position[0] - another_boid.position[0], 2)
+                + math.pow(self.position[1] - another_boid.position[1], 2)) < SIGHT_RANGE:
+            return True
+        return False
+
+    def is_tooClose(self, another_boid):
+        if math.sqrt(math.pow(self.position[0] - another_boid.position[0], 2)
+                + math.pow(self.position[1] - another_boid.position[1], 2)) < TOO_CLOSE_RANGE:
+            return True
+        return False
+
+
+class World(object):
+    # display - zmienna reprezentujaca okno, na ktorym wyswietlamy boidy
+    def __init__(self, number_of_boids, display=None):
+        self.number_of_boids = number_of_boids
+        self.boids = []
+        for i in range(number_of_boids):
+            position = [random.uniform(MIN_POSITION_X, MAX_POSITION_X), random.uniform(MIN_POSITION_Y, MAX_POSITION_Y)]
+            velocity = [random.uniform(MIN_VELOCITY, MAX_VELOCITY), random.uniform(MIN_VELOCITY, MAX_VELOCITY)]
+            self.boids.append(Boid(position, velocity))
+        self.display = display
+
+
+    def __str__(self):
+        return str(self.boids)
+
+    def rule1(self, boid):
+        count = 0.0
+        s = [0.0, 0.0]
+        for boid2 in self.boids:
+            if boid.in_range(boid2):
+                s[0] += boid2.position[0]
+                s[1] += boid2.position[1]
+                count += 1.0
+        if count == 0.0:
+            return [0.0, 0.0]
+        s[0] /= count
+        s[1] /= count
+        s[0] -= boid.position[0]
+        s[1] -= boid.position[1]
+        s[0] /= RULE1_DIVIDER
+        s[1] /= RULE1_DIVIDER
+        return s
+
+    def rule2(self, boid):
+        c = [0, 0]
+        for another_boid in self.boids:
+            if another_boid != boid:
+                if boid.is_tooClose(another_boid):
+                    c[0] = c[0] - ( another_boid.position[0] - boid.position[0])
+                    c[1] = c[1] - ( another_boid.position[1] - boid.position[1])
+        return c
+
+    def rule3(self, boid):
+        count = 0.0
+        s = [0.0, 0.0]
+        for boid2 in self.boids:
+            if boid.in_range(boid2):
+                s[0] += boid2.velocity[0]
+                s[1] += boid2.velocity[1]
+                count += 1.0
+        if count == 0.0:
+            return [0.0, 0.0]
+        s[0] /= count
+        s[1] /= count
+        s[0] /= RULE3_DIVIDER
+        s[1] /= RULE3_DIVIDER
+        return s
+
+    def move_all_boids_to_new_positions(self):
+        for boid in self.boids:
+
+            # kazda z funkcji zwraca tuple ze skladowymi x i y predkosci
+            rule1_velocity = self.rule1(boid)
+            rule2_velocity = self.rule2(boid)
+            rule3_velocity = self.rule3(boid)
+            # print rule1_velocity
+            # print rule2_velocity
+            # print rule3_velocity
+            # print " "
+
+            new_velocity_x = boid.velocity[0] + rule1_velocity[0] + rule2_velocity[0] + rule3_velocity[0]
+            if abs(new_velocity_x) > MAX_VELOCITY:
+                new_velocity_x = math.copysign(MAX_VELOCITY, new_velocity_x)
+            new_x = abs((boid.position[0] + new_velocity_x) % MAX_POSITION_X)
+            boid.position[0] = new_x
+            boid.velocity[0] = new_velocity_x
+
+            new_velocity_y = boid.velocity[1] + rule1_velocity[1] + rule2_velocity[1] + rule3_velocity[1]
+            if abs(new_velocity_y) > MAX_VELOCITY:
+                new_velocity_y = math.copysign(MAX_VELOCITY, new_velocity_y)
+            new_y = abs((boid.position[1] + new_velocity_y) % MAX_POSITION_Y)
+            boid.position[1] = new_y
+            boid.velocity[1] = new_velocity_y
+
+
+    def draw_boids(self):
+        self.display.clean_screen()
+        for i in self.boids:
+            self.display.draw_object(i, "./img/boid-red.png")
+
+
+# Klasa reprezentujaca okno, w ktorym wyswietlamy boidy
+class Display():
+    def __init__(self):
+        self.screen = pygame.display.set_mode((MAX_POSITION_X - MIN_POSITION_X, MAX_POSITION_Y - MIN_POSITION_Y),
+                                              DOUBLEBUF)
+        # flyweight pattern
+        self.image_dictionary = {}
+
+    def draw_object(self, obj, object_image):
+        if not object_image in self.image_dictionary:
+            self.image_dictionary[object_image] = pygame.image.load(object_image)
+        if obj.velocity[0] != 0.0 and obj.velocity[1] != 0.0:
+            rotated = pygame.transform.rotate(self.image_dictionary[object_image],
+                                              -90 - (180 / math.pi) * math.atan2(obj.velocity[1], obj.velocity[0]))
+        self.screen.blit(rotated, (obj.position[0], obj.position[1]))
+        pygame.display.flip()
+
+    def clean_screen(self):
+        self.screen.fill(white)
+
+
+def loop(world):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        world.move_all_boids_to_new_positions()
+        world.draw_boids()
+        # time.sleep(WAIT_TIME) # if we need sleep for slower simulation
+
+
+def start():
+    world = World(50, Display())
+    loop(world)
+
+
+if __name__ == "__main__":
+    IS_RUNNING = True
+    start()
